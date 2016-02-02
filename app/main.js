4576993c695c57ca13d84be7d5f55868441751cf
张珍 <zhang_zhen@Ctrip.com>
2016-02-02 18:21:07 +0800

    update

---
+++ app/main.js
@@ -0,0 +1,8 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Titlecomponent = require('./component.js')
+
+var mainCom = ReactDOM.render(
+	<Titlecomponent/>,
+	document.getElementById('app')
+)
